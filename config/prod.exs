873306dfba261bfb506057cb0a8c8c84936ecc8f
Thibaut <thib@Thibauts-MacBook-Pro.local>
2018-03-29 22:30:45 +0630

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :everlearn, EverlearnWeb.Endpoint,
   load_from_system_env: true,
   server: true, # Without this line, your app will not start the web server!
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "example.com", port: 80],
+  url: [host: "${MY_HOSTNAME}", port: "${PORT}"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :everlearn, Everlearn.Repo,
