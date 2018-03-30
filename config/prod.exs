5e32d0b8e1c590725d4bd0e3056ceb0a5b009355
Thibaut <thib@Thibauts-MacBook-Pro.local>
2018-03-30 09:37:42 +0630

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -4,31 +4,31 @@ config :everlearn,
   secret_key_base: "${SECRET_KEY_BASE}",
   client_id: "${GOOGLE_CLIENT_ID}",
   client_secret: "${GOOGLE_CLIENT_SECRET}",
-  secret_key: "${GUARDIAN_SECRET}"
+  secret_key: "${GUARDIAN_SECRET}",
   host: "${MY_HOSTNAME}",
   port: "${PORT}"
 
 config :everlearn, EverlearnWeb.Endpoint,
   load_from_system_env: true,
   server: true, # Without this line, your app will not start the web server!
-  secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${HOST}",
-        port: "${PORT}"],
+  secret_key_base: ${SECRET_KEY_BASE},
+  url: [host: ${HOST},
+        port: ${PORT}],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :everlearn, Everlearn.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
+  url: ${DATABASE_URL},
   database: "",
   ssl: true,
   pool_size: 1 # Free tier db only allows 1 connection
 
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
-  client_id: "${GOOGLE_CLIENT_ID}",
-  client_secret: "${GOOGLE_CLIENT_SECRET}"
+  client_id: ${GOOGLE_CLIENT_ID},
+  client_secret: ${GOOGLE_CLIENT_SECRET}
 
 config :everlearn, Everlearn.Auth.Guardian,
-  secret_key: "${GUARDIAN_SECRET}"
+  secret_key: ${GUARDIAN_SECRET}
 
 # Do not print debug messages in production
 config :logger, level: :info
